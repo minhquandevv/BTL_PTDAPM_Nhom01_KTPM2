23b46d0c6aff4ab736b14b78635600d41fb1d357
chubich10072003 <100821305+chubich10072003@users.noreply.github.com>
2024-03-02 00:24:54 +0700

    Update managebkInforDetail.blade.php

--- resources/views/frontend/component/customer/managebkInforDetail.blade.php
+++ resources/views/frontend/component/customer/managebkInforDetail.blade.php
@@ -1,56 +1,58 @@
 <section>
-    <form class="container mx-auto bg-white h-100" style="height:100vh">
-        <div class="d-flex">
-            <div class="position-absolute" style="top:113px;left:36px">
-                <i class="fa-solid fa-angle-left fa-2xl"></i>
-            </div>
-            <h2 class="py-5 fw-bold">Xe cắm trại/RV tại Half Moon Bay, California, Hoa Kỳ</h2>
+    <form class="container mx-auto bg-white h-100 p-0" style="height:100vh">
+        <div class="d-flex gap-5 align-items-center my-4">
+            <a href="{{ route('home') }}" class="position-absolute text-dark z-0" style="top:118px;left:45px;">
+                <i class="fa-solid fa-chevron-left"></i>
+            </a>
+            <a href="#" class="text-decoration-none text-dark ">
+                <p class="fs-4 fw-medium">Xe cắm trại/RV tại Half Moon Bay, California, Hoa Kỳ</p>
+            </a>
         </div>
-        <div class="d-flex w-100 mb-5">
-            <div class="w-50 pe-5">
-                <img class="card-img-top rounded-lg" src="https://i.upanh.org/2024/02/27/image_body44710f111b17e14a.png" alt="" style="width:560px,height=420px;" />
+        <div class="d-flex w-100 mb-5 mt-3 ">
+            <div class="pe-3">
+                <img class="card-img-top rounded-lg" src="https://i.upanh.org/2024/02/27/image_body44710f111b17e14a.png" alt="" style="width:650px; height : 465px;" />
             </div>
-            <div class="w-50">
-                <div class="w-100 mb-4">
-                    <img class="card-img-top rounded-lg me-5 " src="https://img.upanh.tv/2024/02/27/image-9.png" alt="" style="width: 309px; height:226px" />
-                    <img class="card-img-top rounded-lg " src="https://img.upanh.tv/2024/02/27/image-11.png" alt="" style="width: 309px; height:226px " />
+            <div>
+                <div>
+                    <img class="card-img-top rounded-lg me-2 mb-3" src="https://img.upanh.tv/2024/02/27/image-9.png" alt="" style="width: 320px; height:225px" />
+                    <img class="card-img-top rounded-lg mb-3" src="https://img.upanh.tv/2024/02/27/image-11.png" alt="" style="width: 320px; height:225px " />
                 </div>
-                <div class="w-100">
-                    <img class="card-img-top rounded-lg me-5" src="https://img.upanh.tv/2024/02/27/image-10.png" alt="" style="width: 309px; height:226px" />
-                    <img class="card-img-top rounded-lg" src="https://img.upanh.tv/2024/02/27/image-12.png" alt="" style="width: 309px; height:226px" />
+                <div>
+                    <img class="card-img-top rounded-lg me-2" src="https://img.upanh.tv/2024/02/27/image-10.png" alt="" style="width: 320px; height:225px" />
+                    <img class="card-img-top rounded-lg" src="https://img.upanh.tv/2024/02/27/image-12.png" alt="" style="width: 320px; height:225px" />
                 </div>
             </div>
         </div>
         <div class="py-5">
             <div>
-                <h1 class="fw-bold">Thông tin phòng</h1>
+                <h2 class="fw-bold">Thông tin phòng</h2>
             </div>
-            <div class="container m-5 ">
+            <div class="container mt-4 p-0 ">
                 <div class="d-flex text-start my-3">
-                    <h3 class="col-lg-2 fw-medium fs-3">Mã phòng:</h3>
-                    <h3 class="col-lg-10 fw-medium fs-3">Mh3001</h3>
+                    <h4 class="col-lg-3 fw-medium fs-3">Mã phòng:</h4>
+                    <h4 class="col-lg-9 fw-medium fs-3">Mh4001</h4>
                 </div>
                 <div class="d-flex text-start my-3">
-                    <h3 class="col-lg-2 fw-medium fs-3">Tên phòng:</h3>
-                    <h3 class="col-lg-10 fw-medium fs-3">Xe cắm trại/ RV</h3>
+                    <h4 class="col-lg-3 fw-medium fs-3">Tên phòng:</h4>
+                    <h4 class="col-lg-9 fw-medium fs-3">Xe cắm trại/ RV</h4>
                 </div>
                 <div class=" d-flex text-start my-3">
-                    <h3 class="col-lg-2 fw-medium fs-3">Giới thiệu phòng:</h3>
-                    <h3 class="col-lg-10 fw-medium fs-3">Khung cảnh lãng mạn riêng tư với tầm nhìn chưa từng có ra bờ
+                    <h4 class="col-lg-3 fw-medium fs-3">Giới thiệu phòng:</h4>
+                    <h4 class="col-lg-9 fw-medium fs-3">Khung cảnh lãng mạn riêng tư với tầm nhìn chưa từng có ra bờ
                         biển
                         và bãi
                         biển
                         Thái Bình Dương.
                         Chỗ ở
-                        sang trọng trong một chiếc RV hoàn toàn mới, gần vịnh Half Moon và Santa Cruz</h3>
+                        sang trọng trong một chiếc RV hoàn toàn mới, gần vịnh Half Moon và Santa Cruz</h4>
                 </div>
                 <div class="d-flex text-start my-3">
-                    <h3 class="col-lg-2 fw-medium fs-3">Trạng thái phòng:</h3>
-                    <h3 class="col-lg-10 fw-medium fs-3">Trống</h3>
+                    <h4 class="col-lg-3 fw-medium fs-3">Trạng thái phòng:</h4>
+                    <h4 class="col-lg-9 fw-medium fs-3">Trống</h4>
                 </div>
                 <div class="d-flex text-start my-3">
-                    <h3 class="col-lg-2 fw-medium fs-3">Đơn giá:</h3>
-                    <h3 class="col-lg-10 fw-medium fs-3">6.800.000VND</h3>
+                    <h4 class="col-lg-3 fw-medium fs-3">Đơn giá:</h4>
+                    <h4 class="col-lg-9 fw-medium fs-3">6.800.000VND</h4>
                 </div>
             </div>
     </form>
