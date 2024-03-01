1e782dc311dbd01ba81e6d351c10fda43831186e
chubich10072003 <100821305+chubich10072003@users.noreply.github.com>
2024-03-02 00:30:23 +0700

    Update bookingHistory.blade.php

--- resources/views/frontend/component/customer/bookingHistory.blade.php
+++ resources/views/frontend/component/customer/bookingHistory.blade.php
@@ -1,16 +1,21 @@
 <section>
     <form class="container mx-auto bg-white" style="height:max-content">
-        <div class="position-absolute" style="top:90px;left:36px">
-            <i class="fa-solid fa-angle-left fa-2xl"></i>
-        </div>
-        <div class="py-3">
-            <h2 class="text-3xl fw-bold text-center py-5"> Lịch sử đặt phòng</h2>
+        <div class="d-flex gap-5 align-items-center my-4 justify-content-center">
+            <a href="{{ route('home') }}" class="position-absolute z-0 text-dark "
+                style="top:118px;left:45px;z-index:0">
+                <i class="fa-solid fa-chevron-left"></i>
+            </a>
+            <div class="text-decoration-none text-dark ">
+                <p class="fs-4 fw-bold text-center">Lịch sử đặt hàng</p>
+            </div>
         </div>
         <div class="border rounded-4 my-5">
-            <div class="mt-4 ms-3 ">
-                <h2 class="fw-bold">Xe cắm trại/RV tại Half Moon Bay, California, Hoa Kỳ</h2>
+            <div class="mt-4 ms-3">
+                <a href="#" class="text-decoration-none text-dark ">
+                    <p class="fs-4 fw-medium">Xe cắm trại/RV tại Half Moon Bay, California, Hoa Kỳ</p>
+                </a>
             </div>
-            <div class="row mx-4 mt-4">
+            <div class="row mx-4 ">
                 <div class="col-md-2 my-3">
                     <a href="#">
                         <img class="card-img-top rounded-lg" src="https://img.upanh.tv/2024/02/27/image-8.png" alt=""
@@ -18,20 +23,24 @@
                     </a>
                 </div>
                 <div class="col-md-7 align-items-center mt-4">
-                    <h3 class="my-4">Mã phòng: MP001</h3>
-                    <h3 class="my-4">Tên Phòng: Xe cắm trại/RV </h3>
-                    <h3 class="my-4">Giá Phòng: 6.800.000VND</h3>
+                    <h5>Mã phòng: MP001</h5>
+                    <h5>Tên Phòng: Xe cắm trại/RV </h5>
+                    <h5>Giá Phòng: 6.800.000VND</h5>
                 </div>
                 <div class="col-3 d-flex justify-content-end align-items-center mb-5 ">
-                    <h2 class="text-secondary me-5">Đã đặt</h2>
+                    <a href="#" class="text-decoration-none text-dark ">
+                        <p class="fs-3 fw-medium text-secondary me-5">Đã đặt</p>
+                    </a>
                 </div>
             </div>
         </div>
         <div class="border rounded-4 my-5">
             <div class="mt-4 ms-3">
-                <h2 class="fw-bold">Biệt thự tại Pacifica, California, Hoa Kỳ</h2>
+                <a href="#" class="text-decoration-none text-dark ">
+                    <p class="fs-4 fw-medium">Biệt thự tại Pacifica, California, Hoa Kỳ</p>
+                </a>
             </div>
-            <div class="row mx-4 mt-4">
+            <div class="row mx-4 ">
                 <div class="col-md-2 my-3">
                     <a href="#">
                         <img class="card-img-top rounded-lg" src="https://img.upanh.tv/2024/02/26/image-25.png" alt=""
@@ -39,20 +48,24 @@
                     </a>
                 </div>
                 <div class="col-md-7 align-items-center mt-4">
-                    <h3 class="my-4">Mã phòng: MP003</h3>
-                    <h3 class="my-4">Tên Phòng: Biệt thự tại Pacifica</h3>
-                    <h3 class="my-4">Giá Phòng: 5.000.000VND</h3>
+                    <h5>Mã phòng: MP003</h5>
+                    <h5>Tên Phòng: Biệt thự tại Pacifica</h5>
+                    <h5>Giá Phòng: 5.000.000VND</h5>
                 </div>
                 <div class="col-3 d-flex justify-content-end align-items-center mb-5 ">
-                    <h2 class="" style="color:red">Đánh giá/phản hồi</h2>
+                    <a href="#" class="text-decoration-none text-dark ">
+                        <p class="fs-3 fw-medium text-secondary me-5 text-danger">Đánh giá/phản hồi</p>
+                    </a>
                 </div>
             </div>
         </div>
-        <div class="border rounded-4 mb-5">
+        <div class="border rounded-4 my-5">
             <div class="mt-4 ms-3">
-                <h2 class="fw-bold">Nhà trên cây tại Half Moon Bay, California, Hoa Kỳ</h2>
+                <a href="#" class="text-decoration-none text-dark ">
+                    <p class="fs-4 fw-medium">Nhà trên cây tại Half Moon Bay, California, Hoa Kỳ</p>
+                </a>
             </div>
-            <div class="row mx-4 mt-4">
+            <div class="row mx-4 ">
                 <div class="col-md-2 my-3">
                     <a href="#">
                         <img class="card-img-top rounded-lg" src="https://img.upanh.tv/2024/02/26/image-31.png" alt=""
@@ -60,12 +73,14 @@
                     </a>
                 </div>
                 <div class="col-md-7 align-items-center mt-4">
-                    <h3 class="my-4">Mã phòng: MP015</h3>
-                    <h3 class="my-4">Tên Phòng: Nhà trên cây tại Half Moon Bay</h3>
-                    <h3 class="my-4">Giá Phòng: 3.000.000VND</h3>
+                    <h5>Mã phòng: MP015</h5>
+                    <h5>Tên Phòng: Nhà trên cây tại Half Moon Bay</h5>
+                    <h5>Giá Phòng: 3.000.000VND</h5>
                 </div>
                 <div class="col-3 d-flex justify-content-end align-items-center mb-5 ">
-                    <h2 class="" style="color:red">Đánh giá/phản hồi</h2>
+                    <a href="#" class="text-decoration-none text-dark ">
+                        <p class="fs-3 fw-medium text-secondary me-5 text-danger">Đánh giá/phản hồi</p>
+                    </a>
                 </div>
             </div>
         </div>
