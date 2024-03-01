05ab99212a834cfebe6c94a4e68592743140dcc6
chubich10072003 <100821305+chubich10072003@users.noreply.github.com>
2024-03-02 00:27:30 +0700

    Update cancelRoomSuccess.blade.php

--- resources/views/frontend/component/customer/cancelRoomSuccess.blade.php
+++ resources/views/frontend/component/customer/cancelRoomSuccess.blade.php
@@ -1,28 +1,34 @@
 <section>
-    <form class="container mx-auto bg-white" style="height:100vh">
-        <div class="position-absolute" style="top:90px;left:36px">
-            <i class="fa-solid fa-angle-left fa-2xl"></i>
-        </div>
-        <div>
-            <h2 class="text-3xl fw-bold text-center py-5"> Đơn đã đặt</h2>
+    <form class="container mx-auto bg-white" style="height:1000px">
+        <div class="d-flex gap-5 align-items-center my-4 justify-content-center">
+            <a href="{{ route('home') }}" class="position-absolute text-dark z-0" style="top:118px;left:45px;">
+                <i class="fa-solid fa-chevron-left"></i>
+            </a>
+            <div class="text-decoration-none text-dark ">
+                <p class="fs-4 fw-bold text-center">Đơn đã đặt</p>
+            </div>
         </div>
         <div class="border rounded-4 my-5">
             <div class="mt-4 ms-3">
-                <h2 class="fw-bold">Xe cắm trại/RV tại Half Moon Bay, California, Hoa Kỳ</h2>
+                <a href="#" class="text-decoration-none text-dark ">
+                    <p class="fs-4 fw-medium">Xe cắm trại/RV tại Half Moon Bay, California, Hoa Kỳ</p>
+                </a>
             </div>
-            <div class="row mx-4 mt-4">
+            <div class="row mx-4 ">
                 <div class="col-md-2 my-3">
                     <a href="#">
                         <img class="card-img-top rounded-lg" src="https://img.upanh.tv/2024/02/27/image-8.png" alt="" style="width: 168px; height:138px" />
                     </a>
                 </div>
                 <div class="col-md-7 align-items-center mt-4">
-                    <h3 class="my-4">Mã phòng: MP001</h3>
-                    <h3 class="my-4">Tên Phòng: Xe cắm trại/RV </h3>
-                    <h3 class="my-4">Giá Phòng: 6.800.000VND</h3>
+                    <h5>Mã phòng: MP001</h5>
+                    <h5>Tên Phòng: Xe cắm trại/RV </h5>
+                    <h5>Giá Phòng: 6.800.000VND</h5>
                 </div>
                 <div class="col-3 d-flex justify-content-end align-items-center mb-5 ">
-                    <h2 class="me-5" style="color:red">Đã hủy</h2>
+                    <a href="#" class="text-decoration-none text-dark ">
+                        <p class="fs-3 fw-medium text-danger me-5">Đã hủy</p>
+                    </a>
                 </div>
             </div>
         </div>
