c6bde59a1c3f156d8bcb2f5b7abf5b76e4d85a46
chubich10072003 <100821305+chubich10072003@users.noreply.github.com>
2024-03-02 00:25:26 +0700

    Update managebkInfor.blade.php

--- resources/views/frontend/component/customer/managebkInfor.blade.php
+++ resources/views/frontend/component/customer/managebkInfor.blade.php
@@ -1,32 +1,45 @@
 <section class="h-100">
     <form class="container mx-auto bg-white" style="height:100vh">
-        <div class="position-absolute" style="top:90px;left:36px">
-            <i class="fa-solid fa-angle-left fa-2xl"></i>
-        </div>
-        <div>
-            <h2 class="text-3xl fw-bold text-center py-5"> Đơn đã đặt</h2>
+        <div class="d-flex gap-5 align-items-center my-4 justify-content-between">
+            <div>
+                <a href="{{ route('home') }}" class="position-absolute text-dark z-0" style="top:118px;left:45px;">
+                    <i class="fa-solid fa-chevron-left"></i>
+                </a>
+            </div>
+            <div>
+                <div href="#" class="text-decoration-none text-dark ">
+                    <p class="fs-4 fw-medium text-center ">Đơn đã đặt</p>
+                </div>
+            </div>
+            <div>
+                <button href="#" class="btn btn-primary bg-danger text-center text-white rounded-2 cursor-pointer border-0" style="background-image: linear-gradient(to right,firebrick 30%, crimson);">
+                    <h5>Lịch sử đặt phòng</h5>
+                </button>
+            </div>
         </div>
         <div class="border rounded-4 my-5">
             <div class="mt-4 ms-3">
-                <h2 class="fw-bold">Xe cắm trại/RV tại Half Moon Bay, California, Hoa Kỳ</h2>
+                <a href="#" class="text-decoration-none text-dark ">
+                    <p class="fs-4 fw-medium">Xe cắm trại/RV tại Half Moon Bay, California, Hoa Kỳ</p>
+                </a>
             </div>
-            <div class="row mx-4 mt-4">
+            <div class="row mx-4 ">
                 <div class="col-md-2 my-3">
                     <a href="#">
                         <img class="card-img-top rounded-lg" src="https://img.upanh.tv/2024/02/27/image-8.png" alt="" style="width: 168px; height:138px" />
                     </a>
                 </div>
                 <div class="col-md-7 align-items-center mt-4">
-                    <h3 class="my-4">Mã phòng: MP001</h3>
-                    <h3 class="my-4">Tên Phòng: Xe cắm trại/RV </h3>
-                    <h3 class="my-4">Giá Phòng: 6.800.000VND</h3>
+                    <h5>Mã phòng: MP001</h5>
+                    <h5>Tên Phòng: Xe cắm trại/RV </h5>
+                    <h5>Giá Phòng: 6.800.000VND</h5>
                 </div>
                 <div class="col-3 d-flex justify-content-end align-items-center mb-5 ">
-                    <h2 class="text-secondary me-5">Chưa thanh toán</h2>
+                    <a href="#" class="text-decoration-none text-dark ">
+                        <p class="fs-3 fw-medium text-secondary me-5">Chưa thanh toán</p>
+                    </a>
                 </div>
             </div>
         </div>
-
-
     </form>
 </section>
