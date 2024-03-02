2dd2db587431de521e91309c2e52d5b20388b0fc
Bui-Hien <buixuanhienmy@gmail.com>
2024-03-02 17:07:41 +0700

    hoan thanh quan ly tai khoan khach hang

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class KhachHangController extends Controller
+class CustomerController extends Controller
 {
     /**
      * Display a listing of the resource.
